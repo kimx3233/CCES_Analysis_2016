4d6e2aa7771561d2bd1535efec085053a54571cb
Dkreitzer <kreitzer.gr@gmail.com>
2019-01-05 09:39:22 -0600

    updates
    New Jupyter Notebook added for query testing only
    Flask app is working (only connected to DB, no visuals yet)

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import func
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -17,18 +19,18 @@ app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/CCES.sqlite"
-db = SQLAlchemy(app)
-
+engine = create_engine("sqlite:///db/CCES_Ver50.sqlite")
+conn = engine.connect()
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
-Base.prepare(db.engine, reflect=True)
+Base.prepare(engine, reflect=True)
+# Save reference to the table
+Cces = Base.classes.CCES_16
+# Create our session (link) from Python to the DB
+session = Session(engine)
 
-# Save references to each table
-Samples_Metadata = Base.classes.sample_metadata
-Samples = Base.classes.samples
+
 
 
 @app.route("/")
@@ -42,8 +44,8 @@ def names():
     """Return a list of sample names."""
 
     # Use Pandas to perform the sql query
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+    stmt = session.query(Cces).statement
+    df = pd.read_sql_query(stmt, session.bind)
 
     # Return a list of the column names (sample names)
     return jsonify(list(df.columns)[2:])
@@ -53,32 +55,24 @@ def names():
 def sample_metadata(sample):
     """Return the MetaData for a given sample."""
     sel = [
-        Samples_Metadata.sample,
+        Cces.GunBackgroundChecks_16
         
         
     ]
 
-    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
+    GBCfor = session.query(*sel).filter(Cces.GunBackgroundChecks_16 == 'Support').count()
+    GBCno = session.query(*sel).filter(Cces.GunBackgroundChecks_16 == 'Oppose').count()
 
     # Create a dictionary entry for each row of metadata information
     sample_metadata = {}
-    for result in results:
-        sample_metadata["sample"] = result[0]
+    sample_metadata["GBCfor"] = GBCfor
+    sample_metadata["GBCno"] = GBCno
+    
         
 
     print(sample_metadata)
     return jsonify(sample_metadata)
 
-
-@app.route("/samples/<sample>")
-def samples(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    
-    }
-    return jsonify(data)
 
 
 if __name__ == "__main__":
