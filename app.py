ba18540410d1eb434166c5c8c99e9f60750ae86b
Dkreitzer <kreitzer.gr@gmail.com>
2019-01-06 13:24:46 -0600

    Updates
    1. Jupyter Notebook testind_db_connection has been updated with functions for Flask App
    2. app.py has been updated with functions from jupyter notebook
    3. Flask app still not responsive

--- app.py
+++ app.py
@@ -15,6 +15,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+#################################################
+# App Contents
+#
+# Database Setup
+# Define Sample Names and Query Addresses
+#
+# Routes
+# Homepage Route (@app.route("/")) - HTML Template
+# Names Route (@app.route("/names")) - JSON
+# Metadata Totals (@app.route("/metadatatotals/<sample>")) - JSON
+# Metadata States Groupy (@app.route("/metadata_states/<sample>")) - JSON
+# Example of hard Coded Query (@app.route("/metadata/GunBC")) - JSON
+#
+# Debugger (not active)
+#################################################
 
 #################################################
 # Database Setup
@@ -30,14 +45,37 @@ Cces = Base.classes.CCES_16
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
+#################################################
+# Define Sample Names and Query Addresses
+#################################################
+# Gun Questions
+GunBackgroundChecks_16 = Cces.GunBackgroundChecks_16
+ProhibitPublication_16 = Cces.ProhibitPublication_16
+BanAssultWeapons_16 = Cces.BanAssultWeapons_16
+MakeCCPEasier_16 = Cces.MakeCCPEasier_16
 
+# Abortion Questions
+AlwaysAllowChoice_16 = Cces.AlwaysAllowChoice_16
+RapeIncestorHealth_16 = Cces.RapeIncestorHealth_16
+ProhibitMoreThan20Weeks_16 = Cces.ProhibitMoreThan20Weeks_16
+Employersdeclinebenefits_16 = Cces.Employersdeclinebenefits_16
+ProhibitFedFunds_16 = Cces.ProhibitFedFunds_16
 
+# Gay Marriage Question
+GayMarriage = Cces.GayMarriage
+
+#################################################
+# Homepage Route
+#################################################
 
 @app.route("/")
 def index():
     """Return the homepage."""
     return render_template("index.html")
 
+#################################################
+# JSON - Column Names Route
+#################################################
 
 @app.route("/names")
 def names():
@@ -47,10 +85,70 @@ def names():
     stmt = session.query(Cces).statement
     df = pd.read_sql_query(stmt, session.bind)
 
-    # Return a list of the column names (sample names)
-    return jsonify(list(df.columns)[2:])
+    # Return a list of the column names (sample names - start with the 8th column since columns 0-7 are respondent profile, not survey responses)
+    return jsonify(list(df.columns)[8:])
 
+#################################################
+# JSON - Totals Route - returning 2, 4, or 6 results
+#################################################
 
+@app.route("/metadatatotals/<sample>")
+def sample_metadatatotals(sample):
+    """Return the totals for a given sample"""
+    TotalResults = []
+    
+    TotalApprove = {}
+    TotalOppose = {}
+    
+    ApproveTemp = session.query(Cces).filter(sample == 'Support').count()
+    TotalApprove['Approve'] = ApproveTemp
+    TotalResults.append(TotalApprove)
+    
+    OpposeTemp = session.query(Cces).filter(sample == 'Oppose').count()
+    TotalOppose['Oppose'] = OpposeTemp
+    TotalResults.append(TotalOppose)
+
+    print(TotalResults)
+    return jsonify(TotalResults)
+
+#################################################
+# JSON - State Totals Route based on Dropdown
+#################################################
+
+@app.route("/metadata_states/<sample>")
+def Metadata_States(sample):
+    
+    # Query to create two lists of tuples (state, number)
+    StatesFor = session.query(Cces.StateName, func.count(sample)).\
+                filter(sample == 'Support').\
+                group_by(Cces.StateName).all()
+    StatesNot = session.query(Cces.StateName, func.count(sample)).\
+                filter(sample == 'Oppose').\
+                group_by(Cces.StateName).all()
+
+    # List that will hold final dictionaries - to be jsonified
+    States_Results = []
+    
+    for i, j in zip(StatesFor, StatesNot):
+
+        tempfile = {}
+        tempfile['State'] = i[0]
+        tempfile['Support'] = i[1]
+        tempfile['Oppose'] = j[1]
+        if i[1] > j[1]:
+            tempoverall = 'Support'
+        else:
+            tempoverall = 'Oppose'
+        tempfile['Overall'] = tempoverall
+
+        States_Results.append(tempfile)
+
+    print(States_Results)
+    return jsonify(States_Results)
+
+#################################################
+# JSON - Example JSON return for a single hard-coded query on Gun Background Checks
+#################################################
 @app.route("/metadata/GunBC")
 def sample_metadata():
 
@@ -79,7 +177,9 @@ def sample_metadata():
     print(SecondTry)
     return jsonify(SecondTry)
 
-
+#################################################
+# Debugger
+#################################################
 
 if __name__ == "__main__":
     app.run()
